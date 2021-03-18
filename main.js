76296cb63f53cbdd8dd94c7da6d8518f8484d252
A-mum <78169290+A-mum@users.noreply.github.com>
2021-03-18 14:24:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+menu_list_array = ["Chicken Tanrooi Pizza", "Veg Supreme Pizza", "Paneer Tikka Pizza", "Deluxe Veggie Pizza",
+"Veg Extravaganza Pizza"];
+function getmenu({
+    var htmldata;
+    htmldata = "<ol class = 'menulist'>
+    menu_list_array.sort();
+    for (var i=0;i<menu_list_array;i++){
+        htmldata=htmldata+ '<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+ "</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata
+
+}
